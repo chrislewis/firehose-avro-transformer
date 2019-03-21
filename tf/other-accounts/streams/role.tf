e4f6a35994c38a7438f7503915ef01cdec5f6b37
Brandon Price <brandon@dacrib.net>
2019-03-21 19:01:23 -0400

    add some notes

--- tf/other-accounts/streams/role.tf
+++ tf/other-accounts/streams/role.tf
@@ -1,6 +1,7 @@
 resource "aws_iam_role" "marketplace-innovation-dev" {
   name = "marketplace-innovation-dev"
 
+#todo; this role needs more like the ability to write to s3 + invoke the lambda function - BP - 
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
